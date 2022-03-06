2613fe6bbb7be48c21516500e355470cbb73cced
Ankitlobo <89775480+Ankitlobo@users.noreply.github.com>
2022-03-06 13:11:34 +0530

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,13 @@
+
+#comparison operator
+from ast import operator
+
+
+b= (14!=7)
+print(b)
+# logical operator
+bool1 = True
+bool2 = False
+print("the value of bool1 and bool2 is", (bool1 and bool2))
+print("the value of bool1 or bool2 is",(bool1 or bool2))
+print("the value of bool1 not bool2 is",( not bool2))
