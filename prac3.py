5ce6e5caabb067b3fc776ccafcf63e963e07311d
Ankitlobo <89775480+Ankitlobo@users.noreply.github.com>
2022-03-08 22:12:10 +0530

    Add files via upload

---
+++ prac3.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
+x=[5,8,10]
+y=[12,16,14]
+x2=[6,9,11]
+y2=[6,15,19]
+plt.plot(x,y,'g',label='Team A',linewidth=1)
+plt.plot(x2,y2,'c',label='Team B',linewidth=5)
+plt.title('Team performance')
+plt.ylabel('Runs')
+plt.xlabel('overs')
+plt.legend()
+plt.show()
