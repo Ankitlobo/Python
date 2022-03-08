68da90d5d4ab49c9c8b23460d9cf0f6e1ac35c28
Ankitlobo <89775480+Ankitlobo@users.noreply.github.com>
2022-03-08 22:23:49 +0530

    Add files via upload

---
+++ prac4.py
@@ -0,0 +1,6 @@
+from matplotlib import pyplot as plt
+import numpy as np
+a= np.array([22,87,5,43,56,73,55,11,20,51,5,79,31,27])
+plt.hist(a,bins =[0,20,40,60,80,100])
+plt.tittle("hystogram")
+plt.show()
